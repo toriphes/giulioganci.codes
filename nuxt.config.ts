d7b071f8e16ca47269aa3e66aadd7100ac4d3491
Giulio Ganci <giulioganci@gmail.com>
2022-07-15 10:06:17 +0200

    feat: add matomo analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,5 +11,30 @@ export default defineNuxtConfig({
     classPrefix: '',
     classSuffix: '',
     dataValue: 'theme'
+  },
+  app: {
+    head: {
+      script: [
+        {
+          hid: 'matomo',
+          children: `
+  <!-- Matomo -->
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//analytics.giulioganci.codes/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '2']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+  <!-- End Matomo Code -->
+          `,
+          type: 'text/javascript'
+        }
+      ]
+    }
   }
 });
